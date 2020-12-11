a3466fc3852bfc7d2cb272699c123bb5d023ac31
Elmounkad Oussama <oelmounkad@gmail.com>
2020-12-11 21:24:23 +0100

    added Character model entity

---
+++ app/src/main/java/com/oelmounkad/gameofthrones/model/Character.java
@@ -0,0 +1,94 @@
+package com.oelmounkad.gotapistarter.model;
+
+import androidx.gameofthrones.Entity;
+import androidx.gameofthrones.PrimaryKey;
+
+
+
+@Entity(tableName = "characters")
+public class Character {
+    @PrimaryKey
+    private int id;
+    private String firstName;
+    private String lastName;
+    private String fullName;
+    private String title;
+    private String family;
+    private String image;
+    private String imageUrl;
+
+    public Character(int id, String firstName, String lastName, String fullName, String title, String family, String image, String imageUrl) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.fullName = fullName;
+        this.title = title;
+        this.family = family;
+        this.image = image;
+        this.imageUrl = imageUrl;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getFamily() {
+        return family;
+    }
+
+    public void setFamily(String family) {
+        this.family = family;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+}
