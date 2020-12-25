68e5caa5610f5f134bd54a855e1af29f814e29b4
Elmounkad Oussama <oelmounkad@gmail.com>
2020-12-25 03:04:08 +0100

    bugfix

--- app/src/main/java/com/oelmounkad/gameofthrones/db/CharacterDB.java
+++ app/src/main/java/com/oelmounkad/gameofthrones/db/CharacterDB.java
@@ -4,7 +4,7 @@ import androidx.room.RoomDatabase;
 import androidx.room.Database;
 import com.oelmounkad.gameofthrones.model.Character;
 
-@Database(entities = {Character.class},version = 3,exportSchema = false)
+@Database(entities = {Character.class},version = 10,exportSchema = false)
 public abstract class CharacterDB extends RoomDatabase {
     public abstract CharDao charDao();
 }
